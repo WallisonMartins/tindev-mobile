52394e65884ea7cb72222de03b0cd5956ddfa6c2
Wallison Martins <wallisonpinheiro1@gmail.com>
2019-08-18 10:33:19 -0300

    base-aplication-mobile

---
+++ src/pages/Login.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+import { KeyboardAvoidingView, Platform, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
+
+import logo from '../assets/logo.png';
+
+export default function Login() {
+  return (
+    <KeyboardAvoidingView
+    behavior="padding"
+    enabled={Platform.OS === 'ios'}
+    style={styles.container}
+    >
+      <Image source={logo} />
+
+      <TextInput
+      autoCapitalize="none"
+      autoCorrect={false}
+      placeholder="Digite seu usuário no Github"
+      placeholderTextColor="#999"
+      style={styles.input}
+      />
+
+      <TouchableOpacity style={styles.button}>
+        <Text styles={styles.buttonText}>Enviar</Text>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 30
+  },
+
+  input: {
+    height: 46,
+    alignSelf: 'stretch',
+    backgroundColor: '#FFF',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 4,
+    marginTop: 20,
+    paddingHorizontal: 15,
+  },
+
+  button: {
+    height: 46,
+    alignSelf: 'stretch',
+    backgroundColor: '#DF4723',
+    borderRadius: 4,
+    marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
