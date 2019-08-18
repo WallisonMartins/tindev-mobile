b6fe39c7bfb42fe7a12da85e48104ef20b5add0c
Wallison Martins <wallisonpinheiro1@gmail.com>
2019-08-18 14:04:21 -0300

    first test promise from api

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { KeyboardAvoidingView, Platform, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
 
+import api from '../services/api';
+
 import logo from '../assets/logo.png';
 
-export default function Login() {
+export default function Login({ navigation }) {
+  const [user, setUser] = useState('');
+
+  async function handleLogin() {
+    const response = await api.post('/devs', { username: user });
+
+    const { _id } = response.data;
+
+    console.log(_id); 
+
+    navigation.navigate('Main', { _id }); 
+  }
+
   return (
     <KeyboardAvoidingView
     behavior="padding"
@@ -19,9 +33,11 @@ export default function Login() {
       placeholder="Digite seu usuário no Github"
       placeholderTextColor="#999"
       style={styles.input}
+      value={user}
+      onChangeText={setUser}
       />
 
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity onPress={handleLogin} style={styles.button}>
         <Text styles={styles.buttonText}>Enviar</Text>
       </TouchableOpacity>
     </KeyboardAvoidingView>
